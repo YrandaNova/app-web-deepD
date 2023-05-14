344e05199ff9f266bdcb66bc97c7239b127a916b
Yranda <mfcr112000@gmail.com>
2023-05-13 18:50:18 -0600

    python code that makes a watermark and changes a pdf as well as pdf example

---
+++ changepdf.py
@@ -0,0 +1,52 @@
+
+from PyPDF2 import PdfReader, PdfReader, PdfWriter
+
+input_pdf='/home/yranda/Documents/Deep_dive/Lorem.pdf'
+output='/home/yranda/Documents/Deep_dive/Resources'
+watermark='Resources/watermark.png'
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4
+
+
+#text = input("Enter the watermark text here:")
+text="Gracias te amo"
+pdf_file='Lorem.pdf'
+
+def makepdf(pdf_file):
+    watermark = 'watermark.pdf'
+    merged = "Watermarked.pdf"
+
+    with open(pdf_file, "rb") as input_file, open(watermark, "rb") as watermark_file:
+        input_pdf = PdfReader(input_file)
+        watermark_pdf = PdfReader(watermark_file)
+        watermark_page = watermark_pdf.pages[0]
+        output = PdfWriter()
+
+        for i in range(len(input_pdf.pages)):
+            pdf_page = input_pdf.pages[i]
+            pdf_page.merge_page(watermark_page)
+
+            output.add_page(pdf_page)
+
+        with open(merged, "wb") as merged_file:
+            output.write(merged_file)
+
+
+
+
+def makeWatermark(text):   
+    pdf = canvas.Canvas("watermark.pdf", pagesize=A4)
+    pdf.translate(inch, inch)
+    pdf.setFillColor(colors.grey, alpha=0.6)
+    pdf.setFont("Helvetica", 50)
+    pdf.rotate(45)
+    pdf.drawCentredString(400, 100, text)
+    pdf.save()
+
+
+
+
+makeWatermark(text)
+makepdf(pdf_file)
